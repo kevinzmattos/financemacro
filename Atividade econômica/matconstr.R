0e2fb02e74acca16c79172aa9ec014bf74e459d0
Kevin Mattos <51105838+kevinzmattos@users.noreply.github.com>
2021-04-15 10:25:35 -0300

    Rename matconstr.R to Atividade econômica/matconstr.R

--- Atividade econômica/matconstr.R
+++ Atividade econômica/matconstr.R
@@ -1,13 +1,13 @@
 mat_constr <- 
-  get_sidra(api='//t/3415/n1/all/v/1195/p/all/c11046/40312/d/v1195%201') %>% # Pegando a tabela atrav√©s do API do SIDRA
-  mutate(Data = parse_date(`MÍs (CÛdigo)`, format = '%Y%m')) %>% # transformando o objeto em formato de data
+  get_sidra(api='//t/3415/n1/all/v/1195/p/all/c11046/40312/d/v1195%201') %>% # Pegando a tabela atrav√É¬©s do API do SIDRA
+  mutate(Data = parse_date(`M√™s (C√≥digo)`, format = '%Y%m')) %>% # transformando o objeto em formato de data
   select(Data, Valor) %>% # seleciona apenas as colunas discriminadas
   as_tibble() # deixa no formato ideal para analise de dados
 
 
 
 teste <- filter(mat_constr, month(Data) %in% c(1,5,9))
-## Plotando o gr√°fico
+## Plotando o gr√É¬°fico
 
 filter(mat_constr, Data > '2011-12-01') %>%
   ggplot(aes(x=Data, y =Valor)) +
@@ -17,6 +17,6 @@ filter(mat_constr, Data > '2011-12-01') %>%
   coord_cartesian(xlim = c(as.Date("2012-03-01"),as.Date("2020-07-01")))+
   theme(axis.text.x = element_text(angle=90))+
   labs(x='', y='', 
-       title='Materiais de construÁ„o: volume de vendas do comÈrcio varejista',
-       subtitle = 'Õndice base fixa com ajuste sazonal (Base: 2014 = 100) (n˙mero Ìndice)',
-       caption='Fonte: IBGE - ElaboraÁ„o: UFABC Finance')
+       title='Materiais de constru√ß√£o: volume de vendas do com√©rcio varejista',
+       subtitle = '√çndice base fixa com ajuste sazonal (Base: 2014 = 100) (n√∫mero √≠ndice)',
+       caption='Fonte: IBGE - Elabora√ß√£o: UFABC Finance')
